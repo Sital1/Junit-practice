f64d64a095e602d776fde4f850894f9143471757
Sital Sitoula <sitalsitoula12@gmail.com>
2021-08-12 00:39:23 -0500

    added first test class and ran a test

---
+++ src/main/java/practice/junit/MathUtils.java
@@ -0,0 +1,10 @@
+package practice.junit;
+
+public class MathUtils {
+
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+}
---
+++ src/test/java/practice/junit/MathUtilsTest.java
@@ -0,0 +1,16 @@
+package practice.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    // lets junit know this is a test
+    @Test
+    void test()
+    {
+        System.out.println("This test ran");
+    }
+
+}
